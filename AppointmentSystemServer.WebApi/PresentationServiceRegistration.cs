3e576218445bc8f0c22ec1564f6d91e628f158b5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-23 03:50:49 +0300

    📝 Cors, Redis ve Jwt yapılandırması yapıldı.

---
+++ AppointmentSystemServer.WebApi/PresentationServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using AppointmentSystemServer.Infrastructure.Configurations;
+
+namespace AppointmentSystemServer.WebApi;
+
+public static class PresentationServiceRegistration
+{
+    public const string AllowSpecificOrigins = "AllowSpecificOrigins";
+
+    public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(AllowSpecificOrigins, policy =>
+            {
+                policy.WithOrigins("http://localhost:4200") // Güvenilir domainleri ekle           
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowCredentials();
+            });
+        });
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = "localhost:6379";
+            options.InstanceName = "AppointmentSystemCache:";
+        });
+
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+        return services;
+    }
+}
---
+++ AppointmentSystemServer.WebApi/Program.cs
@@ -0,0 +1,33 @@
+using AppointmentSystemServer.Application;
+using AppointmentSystemServer.Infrastructure;
+using AppointmentSystemServer.Persistence;
+using AppointmentSystemServer.WebApi;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPresentation(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddPersistence(builder.Configuration);
+
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.MapOpenApi();
+
+app.UseCors(PresentationServiceRegistration.AllowSpecificOrigins);
+
+app.MapScalarApiReference();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
