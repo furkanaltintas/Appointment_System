764f5f78ea92d24516cf1d7fbaccebdfa7ebffb2
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-23 03:40:52 +0300

    🔧 Gereksiz Identity sınıfları kaldırıldı.

---
+++ AppointmentSystemServer.Persistence/Helpers/IdentityModelBuilderHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentSystemServer.Persistence.Helpers;
+public static class IdentityModelBuilderHelper
+{
+    public static void IgnoreIdentityEntities(ModelBuilder builder)
+    {
+        var identityEntities = new[]
+        {
+            typeof(IdentityUserClaim<Guid>),
+            typeof(IdentityRoleClaim<Guid>),
+            typeof(IdentityUserLogin<Guid>),
+            typeof(IdentityUserToken<Guid>)
+        };
+
+        foreach (var identityEntity in identityEntities)
+            builder.Ignore(identityEntity);
+    }
+}
