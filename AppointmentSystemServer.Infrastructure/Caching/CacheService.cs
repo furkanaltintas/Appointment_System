7281bc31b20e8c50d303b52753fa31f2ba33ac78
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-23 03:45:47 +0300

    ✨ Cache mekanizması yapıldı

---
+++ AppointmentSystemServer.Infrastructure/Caching/CacheService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace AppointmentSystemServer.Infrastructure.Caching;
+
+public class CacheService(IDistributedCache distributedCache) : ICacheService
+{
+    public async Task<T> GetOrSetAsync<T>(string cacheKey, Func<Task<T>> fetchData, TimeSpan? absoluteExpiration = null)
+    {
+        // Cache'ten veriyi al
+        string? cachedData = await distributedCache.GetStringAsync(cacheKey);
+
+        if (!string.IsNullOrEmpty(cachedData))
+        {
+            // Cache'ten veri varsa deserialize et
+            return JsonSerializer.Deserialize<T>(cachedData);
+        }
+
+        // Cache'te veri yoksa, fetchData fonksiyonu ile veriyi al
+        var data = await fetchData();
+
+        // Cache'e veriyi serialize edip ekle
+        var serializedData = JsonSerializer.Serialize(data);
+
+        // Cache süresini belirleyin
+        DistributedCacheEntryOptions cacheOptions = new()
+        {
+            AbsoluteExpirationRelativeToNow = absoluteExpiration ?? TimeSpan.FromMinutes(20)  // Varsayılan 20 dakika
+        };
+
+        await distributedCache.SetStringAsync(cacheKey, serializedData, cacheOptions);
+        return data;
+    }
+
+    public async Task RemoveAsync(string cacheKey) => await distributedCache.RemoveAsync(cacheKey);
+}
---
+++ AppointmentSystemServer.Infrastructure/Caching/ICacheService.cs
@@ -0,0 +1,7 @@
+﻿namespace AppointmentSystemServer.Infrastructure.Caching;
+
+public interface ICacheService
+{
+    Task<T> GetOrSetAsync<T>(string cacheKey, Func<Task<T>> fetchData, TimeSpan? absoluteExpiration = null);
+    Task RemoveAsync(string cacheKey);
+}
