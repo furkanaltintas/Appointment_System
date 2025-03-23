7f35253c0dc372399d0bea2b14de7b1465d43607
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-23 03:47:03 +0300

    📝 CacheService yapılandırması yapıldı

---
+++ AppointmentSystemServer.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using AppointmentSystemServer.Infrastructure.Caching;
+using AppointmentSystemServer.Infrastructure.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppointmentSystemServer.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<ICacheService, CacheService>();
+        return services;
+    }
+}
