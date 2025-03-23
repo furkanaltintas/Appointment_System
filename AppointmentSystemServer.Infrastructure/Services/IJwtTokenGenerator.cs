a3406217d06a63c05abfd18e9a9988847a6f37a5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-23 03:46:10 +0300

    ✨ Jwt yapılandırması yapıldı

---
+++ AppointmentSystemServer.Infrastructure/Services/IJwtTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using AppointmentSystemServer.Domain.Entities;
+
+namespace AppointmentSystemServer.Infrastructure.Services;
+
+public interface IJwtTokenGenerator
+{
+    string CreateToken(AppUser user);
+}
---
+++ AppointmentSystemServer.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,48 @@
+﻿using AppointmentSystemServer.Domain.Entities;
+using AppointmentSystemServer.Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AppointmentSystemServer.Infrastructure.Services;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenGenerator(IOptions<JwtSettings> jwtSettings)
+    {
+        _jwtSettings = jwtSettings.Value;
+    }
+
+    public string CreateToken(AppUser user)
+    {
+        DateTime expires = DateTime.UtcNow.AddDays(_jwtSettings.ExpiryInDays);
+
+        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+        SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha512);
+
+        JwtSecurityToken securityToken = new(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: expires,
+            claims: GetClaims(user),
+            notBefore: DateTime.UtcNow,
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+
+    private IEnumerable<Claim> GetClaims(AppUser user)
+    {
+        return new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Name, user.FullName),
+            new(ClaimTypes.Email, user.Email ?? string.Empty),
+            new(ClaimTypes.Role, string.Empty)
+        };
+    }
+}
