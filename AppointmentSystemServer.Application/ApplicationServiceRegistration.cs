b282ff3492d548218dbecab252213bd2469eba12
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-23 03:49:01 +0300

    📝 AutoMapper ve MediatR yapılandırması yapıldı.

---
+++ AppointmentSystemServer.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+namespace AppointmentSystemServer.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        services.AddAutoMapper(assembly);
+        services.AddMediatR(configuration => { configuration.RegisterServicesFromAssembly(assembly); });
+        return services;
+    }
+}
