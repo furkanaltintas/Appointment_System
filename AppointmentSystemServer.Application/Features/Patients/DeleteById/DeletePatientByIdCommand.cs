e3dc84d93238e27c510cf377a5a4497ae76f9904
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:21:15 +0300

    ✨ Patient DeleteById işlemi yapıldı.

---
+++ AppointmentSystemServer.Application/Features/Patients/DeleteById/DeletePatientByIdCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Patients.DeleteById;
+
+public record DeletePatientByIdCommand(string Id) : IRequest<Result<string>>;
---
+++ AppointmentSystemServer.Application/Features/Patients/DeleteById/DeletePatientByIdCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AppointmentSystemServer.Application.Features.Patients._Constants;
+using AppointmentSystemServer.Application.Services.Repositories;
+using AppointmentSystemServer.Domain.Entities;
+using AppointmentSystemServer.Infrastructure.Caching;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Patients.DeleteById;
+
+public class DeletePatientByIdCommandHandler(
+    IPatientRepository patientRepository,
+    IUnitOfWork unitOfWork,
+    ICacheService cacheService) : IRequestHandler<DeletePatientByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeletePatientByIdCommand request, CancellationToken cancellationToken)
+    {
+        Patient? patient = await patientRepository.GetByExpressionAsync(d => d.Id == int.Parse(request.Id));
+        if (patient == null) return Result<string>.Failure(PatientConstants.NoSuch);
+        patientRepository.Delete(patient);
+        await unitOfWork.SaveChangesAsync();
+
+        await cacheService.RemoveByPrefixAsync(new() { PatientConstants.CacheKey, PatientConstants.CacheKeyGetByIdentityNumber(patient.IdentityNumber) });
+        return PatientConstants.DeleteMessage;
+    }
+}
