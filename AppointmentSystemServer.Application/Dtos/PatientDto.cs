e44577b98b6b0f1b620f5e33a56b09890aa9fd32
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:17:46 +0300

    ✨ Dto sınıfı oluşturuldu

---
+++ AppointmentSystemServer.Application/Dtos/PatientDto.cs
@@ -0,0 +1,34 @@
+﻿namespace AppointmentSystemServer.Application.Dtos;
+
+public class PatientDto
+{
+    public PatientDto()
+    {
+        FirstName = string.Empty;
+        LastName = string.Empty;
+        IdentityNumber = string.Empty;
+        City = string.Empty;
+        Town = string.Empty;
+        FullAddress = string.Empty;
+    }
+
+    public PatientDto(int id, string firstName, string lastName, string ıdentityNumber, string city, string town, string fullAddress)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        IdentityNumber = ıdentityNumber;
+        City = city;
+        Town = town;
+        FullAddress = fullAddress;
+    }
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName => $"{FirstName} {LastName}";
+    public string IdentityNumber { get; set; }
+    public string City { get; set; }
+    public string Town { get; set; }
+    public string FullAddress { get; set; }
+}
