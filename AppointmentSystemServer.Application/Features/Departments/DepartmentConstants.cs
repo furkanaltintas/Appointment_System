40a589f5e54db5be836ee46a0c305b5393500eb3
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:23:39 +0300

    ✨ Mapping ve Const işlemi yapıldı

--- AppointmentSystemServer.Application/Features/Departments/DepartmentConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AppointmentSystemServer.Application.Features.Departments;
-
-public static class DepartmentConstants
-{
-    public static string CacheKey = "GetAllDepartmentResponse";
-
-    public static string NotFound = "Department not found.";
-}
