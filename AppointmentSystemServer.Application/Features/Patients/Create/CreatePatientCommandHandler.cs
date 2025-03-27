5a9dd7c7f18e48d3f3553521cc956b057bf935f6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:21:06 +0300

    ✨ Patient Create işlemi yapıldı.

---
+++ AppointmentSystemServer.Application/Features/Patients/Create/CreatePatientCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AppointmentSystemServer.Application.Features.Patients._Constants;
+using AppointmentSystemServer.Application.Services.Repositories;
+using AppointmentSystemServer.Domain.Entities;
+using AppointmentSystemServer.Infrastructure.Caching;
+using AutoMapper;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Patients.Create;
+
+public class CreatePatientCommandHandler(
+    IPatientRepository patientRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper,
+    ICacheService cacheService) : IRequestHandler<CreatePatientCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(CreatePatientCommand request, CancellationToken cancellationToken)
+    {
+        Boolean patientExist = patientRepository.Any(p => p.IdentityNumber == request.IdentityNumber);
+        if (patientExist) return Result<string>.Failure(PatientConstants.IdentityNumberAlreadyUse);
+
+        Patient patient = mapper.Map<Patient>(request);
+        await patientRepository.AddAsync(patient, cancellationToken);
+        await unitOfWork.SaveChangesAsync();
+
+        await cacheService.RemoveAsync(PatientConstants.CacheKey);
+        return PatientConstants.CreateMessage;
+    }
+}
