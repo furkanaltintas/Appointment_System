4546bc24d200900bffce20e565ddaab76a03b5ca
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:24:13 +0300

    Appointment özel iş kuralları yazıldı

--- AppointmentSystemServer.Application/ApplicationServiceRegistration.cs
+++ AppointmentSystemServer.Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AppointmentSystemServer.Application.Features.Appointments._Rules;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 namespace AppointmentSystemServer.Application;
 
@@ -7,6 +8,9 @@ public static class ApplicationServiceRegistration
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         Assembly assembly = Assembly.GetExecutingAssembly();
+
+        services.AddScoped(typeof(AppointmentBusinessRules));
+
 
         services.AddAutoMapper(assembly);
         services.AddMediatR(configuration => { configuration.RegisterServicesFromAssembly(assembly); });
---
+++ AppointmentSystemServer.Application/Features/Appointments/_Rules/AppointmentBusinessRules.cs
@@ -0,0 +1,33 @@
+﻿using AppointmentSystemServer.Application.Features.Appointments._Constants;
+using AppointmentSystemServer.Application.Services.Repositories;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Appointments._Rules;
+
+public class AppointmentBusinessRules(IAppointmentRepository appointmentRepository)
+{
+    public async Task<Result<string>> ValidateAppointmentDate(int doctorId, DateTime startDate, DateTime endDate, CancellationToken cancellationToken)
+    {
+        DateTime now = DateTime.Now;
+
+        if (endDate < now) return Result<string>.Failure(AppointmentConstants.ValidateAppointmentDate);
+
+        return await IsAppointmentDateNotAvailable(doctorId, startDate, endDate, cancellationToken)
+            ? Result<string>.Failure(AppointmentConstants.DateIsNotAvailable)
+            : "";
+    }
+
+    private async Task<bool> IsAppointmentDateNotAvailable(int doctorId, DateTime startDate, DateTime endDate, CancellationToken cancellationToken)
+    {
+        bool isAppointmentDateNotAvailable = await appointmentRepository
+            .AnyAsync
+            (a => a.DoctorId == doctorId &&
+            (a.StartDate < endDate && a.StartDate >= startDate) || // Mevcut randevunun bitişi, diğer randevunun başlangıcıyla çakışıyor mu ?
+            (a.EndDate > startDate && a.EndDate < endDate) || // Mevcut randevunun başlangıcı, diğer randevunun bitişiyle çakışıyor mu ?
+            (a.StartDate >= startDate && a.EndDate <= endDate) || // Mevcut randevu, diğer randevu içinde mi tamamen
+            (a.StartDate <= startDate && a.EndDate >= endDate),
+            cancellationToken); // Mevcut randevu, diğer randevuyu tamamen kapsıyor mu
+
+        return isAppointmentDateNotAvailable;
+    }
+}
