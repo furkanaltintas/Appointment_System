97b3140821d28f1d38a4dfa3398f9289481d77bb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:20:51 +0300

    ✨ Mapping ve Const yapıları oluşturuldu

--- AppointmentSystemServer.Application/Features/Doctors/_Mappings/DoctorMappingsProfiles.cs
+++ AppointmentSystemServer.Application/Features/Doctors/_Mappings/DoctorMappingsProfiles.cs
@@ -4,20 +4,25 @@ using AppointmentSystemServer.Application.Features.Doctors.Update;
 using AppointmentSystemServer.Domain.Entities;
 using AutoMapper;
 
-namespace AppointmentSystemServer.Application.Features.Doctors;
+namespace AppointmentSystemServer.Application.Features.Doctors._Mappings;
 
 public class DoctorMappingsProfiles : Profile
 {
     public DoctorMappingsProfiles()
     {
         CreateMap<Doctor, GetAllDoctorResponse>()
-            .ForMember(opt => opt.DepartmentName, dest => dest.MapFrom(src => src.Department.Name))
+            .ForMember(dest => dest.Department, opt => opt.MapFrom(src => src.Department))
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()))
             .ReverseMap();
+
 
         CreateMap<Doctor, CreateDoctorCommand>()
             .ReverseMap();
 
+
         CreateMap<Doctor, UpdateDoctorCommand>()
+            .ReverseMap()
+            .ForMember(dest => dest.FullName, opt => opt.Ignore())
             .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
     }
 }
