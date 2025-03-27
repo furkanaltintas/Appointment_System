d9c56b6eb1a31ec86d85835e5cfee601265d3726
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:22:00 +0300

    ✨ Mapping ve Const yapısı oluşturuldu

---
+++ AppointmentSystemServer.Application/Features/Patients/_Constants/PatientConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace AppointmentSystemServer.Application.Features.Patients._Constants;
+
+public static class PatientConstants
+{
+    public const string CacheKey = "Patient:GetAll";
+    public static string CacheKeyGetByIdentityNumber(string identityNumber) => $"Patient:GetByIdentityNumber:{identityNumber}";
+
+
+    public const string NoSuch = "There is no such patient in the system.";
+    public const string NotFound = "Patient not found.";
+    public const string IdentityNumberAlreadyUse = "This identity number already use";
+
+
+    public const string CreateMessage = "Patient create is successful";
+    public const string UpdateMessage = "Patient update is successful";
+    public const string DeleteMessage = "Patient delete is successful";
+}
---
+++ AppointmentSystemServer.Application/Features/Patients/_Mappings/PatientMappingsProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AppointmentSystemServer.Application.Features.Patients.Create;
+using AppointmentSystemServer.Application.Features.Patients.Update;
+using AppointmentSystemServer.Domain.Entities;
+using AutoMapper;
+
+namespace AppointmentSystemServer.Application.Features.Patients._Mappings;
+
+public class PatientMappingsProfiles : Profile
+{
+    public PatientMappingsProfiles()
+    {
+        CreateMap<Patient, CreatePatientCommand>()
+            .ReverseMap();
+
+        CreateMap<Patient, UpdatePatientCommand>()
+            .ReverseMap()
+            .ForMember(dest => dest.FullName, opt => opt.Ignore())
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+    }
+}
