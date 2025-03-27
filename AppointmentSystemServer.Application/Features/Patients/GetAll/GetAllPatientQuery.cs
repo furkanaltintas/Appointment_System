120a7b79c3b985f8d119f43c86eead39a840bc38
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:21:22 +0300

    ✨ Patient GetAll işlemi yapıldı.

---
+++ AppointmentSystemServer.Application/Features/Patients/GetAll/GetAllPatientQuery.cs
@@ -0,0 +1,7 @@
+﻿using AppointmentSystemServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Patients.GetAll;
+
+public record GetAllPatientQuery() : IRequest<Result<List<Patient>>>;
