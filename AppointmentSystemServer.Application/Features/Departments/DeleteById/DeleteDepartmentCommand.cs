e66ad7007f34a4aa85b59c0a2cec1feaff2faae9
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:23:01 +0300

    ✨ Department DeleteById işlemi yapıldı

---
+++ AppointmentSystemServer.Application/Features/Departments/DeleteById/DeleteDepartmentCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Departments.DeleteById;
+
+public record DeleteDepartmentCommand(int Id) : IRequest<Result<string>>;
