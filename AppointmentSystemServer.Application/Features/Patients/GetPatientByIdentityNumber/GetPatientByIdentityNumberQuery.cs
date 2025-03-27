6587d4ef9c00e8dbaa55a0008b668b897f3e7c67
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-28 01:21:33 +0300

    ✨ Patient GetPatient işlemi yapıldı.

---
+++ AppointmentSystemServer.Application/Features/Patients/GetPatientByIdentityNumber/GetPatientByIdentityNumberQuery.cs
@@ -0,0 +1,7 @@
+﻿using AppointmentSystemServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Patients.GetPatientByIdentityNumber;
+
+public record GetPatientByIdentityNumberQuery(string IdentityNumber) : IRequest<Result<Patient>>;
---
+++ AppointmentSystemServer.Application/Features/Patients/GetPatientByIdentityNumber/GetPatientByIdentityNumberQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AppointmentSystemServer.Application.Features.Patients._Constants;
+using AppointmentSystemServer.Application.Services.Repositories;
+using AppointmentSystemServer.Domain.Entities;
+using AppointmentSystemServer.Infrastructure.Caching;
+using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Patients.GetPatientByIdentityNumber;
+
+class GetPatientByIdentityNumberQueryHandler(IPatientRepository patientRepository, ICacheService cacheService) : IRequestHandler<GetPatientByIdentityNumberQuery, Result<Patient>>
+{
+    public async Task<Result<Patient>> Handle(GetPatientByIdentityNumberQuery request, CancellationToken cancellationToken)
+    {
+        var patient = await cacheService.GetOrSetAsync(PatientConstants.CacheKeyGetByIdentityNumber(request.IdentityNumber), async () =>
+        {
+            return await patientRepository.GetByExpressionAsync(p => p.IdentityNumber == request.IdentityNumber, cancellationToken);
+        });
+
+        return patient;
+    }
+}
