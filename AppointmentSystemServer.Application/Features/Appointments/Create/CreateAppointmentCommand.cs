e8d926f9a12a708205f6639e73c130defdc6ff64
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:30:13 +0300

    CreateAppointment oluşturuldu.

---
+++ AppointmentSystemServer.Application/Features/Appointments/Create/CreateAppointmentCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Appointments.Create;
+
+public record CreateAppointmentCommand(
+    string StartDate,
+    string EndDate,
+    int DoctorId,
+    int PatientId,
+    string FirstName,
+    string LastName,
+    string IdentityNumber,
+    string City,
+    string Town,
+    string FullAddress) : IRequest<Result<string>>;
