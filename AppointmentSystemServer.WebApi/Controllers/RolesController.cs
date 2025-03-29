04132b363aca9a89a338bc4d9eafda25d4d3a576
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:36:48 +0300

    Role işlemleri yazıldı

---
+++ AppointmentSystemServer.WebApi/Controllers/RolesController.cs
@@ -0,0 +1,33 @@
+﻿using AppointmentSystemServer.Application.Features.Roles.GetAllRolesForUsers;
+using AppointmentSystemServer.Application.Features.Roles.RoleSync;
+using AppointmentSystemServer.Domain.Entities;
+using AppointmentSystemServer.WebApi.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TS.Result;
+
+namespace AppointmentSystemServer.WebApi.Controllers;
+
+[AllowAnonymous]
+public class RolesController : BaseController
+{
+    public RolesController(IMediator mediator) : base(mediator) { }
+
+
+    [HttpGet]
+    public async Task<IActionResult> Sync(CancellationToken cancellationToken)
+    {
+        RoleSyncCommand roleSyncCommand = new();
+        Result<string> result = await _mediator.Send(roleSyncCommand, cancellationToken);
+        return StatusCode(result.StatusCode, result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllRoles(CancellationToken cancellationToken)
+    {
+        GetAllRoleQuery getAllRolesForUsersQuery = new();
+        Result<List<AppRole>> resultListDoctors = await _mediator.Send(getAllRolesForUsersQuery, cancellationToken);
+        return StatusCode(resultListDoctors.StatusCode, resultListDoctors);
+    }
+}
