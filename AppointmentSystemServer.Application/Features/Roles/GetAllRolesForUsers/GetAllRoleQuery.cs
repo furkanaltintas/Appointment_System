f615e65162202d9caca5d9a42598a602eede23fa
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:33:59 +0300

    GetAll Role

---
+++ AppointmentSystemServer.Application/Features/Roles/GetAllRolesForUsers/GetAllRoleQuery.cs
@@ -0,0 +1,7 @@
+﻿using AppointmentSystemServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Roles.GetAllRolesForUsers;
+
+public record GetAllRoleQuery() : IRequest<Result<List<AppRole>>>;
