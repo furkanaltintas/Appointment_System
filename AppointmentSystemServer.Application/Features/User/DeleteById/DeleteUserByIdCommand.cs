c7b27193a23a7ee929992cd67e6665c7201565fa
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:34:49 +0300

    DeleteById User

---
+++ AppointmentSystemServer.Application/Features/User/DeleteById/DeleteUserByIdCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.User.DeleteById;
+
+public record DeleteUserByIdCommand(Guid Id) : IRequest<Result<string>>;
