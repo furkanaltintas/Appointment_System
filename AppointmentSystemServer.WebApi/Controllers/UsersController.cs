7272a06552255c2a7344706d06befbdaa073bb64
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:36:37 +0300

    User işlemleri yazıldı

---
+++ AppointmentSystemServer.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using AppointmentSystemServer.Application.Features.Roles.GetAllRolesForUsers;
+using AppointmentSystemServer.Application.Features.User.Create;
+using AppointmentSystemServer.Application.Features.User.DeleteById;
+using AppointmentSystemServer.Application.Features.User.GetAll;
+using AppointmentSystemServer.Application.Features.User.Update;
+using AppointmentSystemServer.Domain.Entities;
+using AppointmentSystemServer.WebApi.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TS.Result;
+
+namespace AppointmentSystemServer.WebApi.Controllers;
+
+[AllowAnonymous]
+public class UsersController : BaseController
+{
+    public UsersController(IMediator mediator) : base(mediator) { }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        GetAllUserQuery getAllUserQuery = new();
+        Result<List<GetAllUserQueryResponse>> result = await _mediator.Send(getAllUserQuery, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateUserCommand createUserCommand)
+    {
+        Result<string> result = await _mediator.Send(createUserCommand);
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateUserCommand updateUserCommand)
+    {
+        Result<string> result = await _mediator.Send(updateUserCommand);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteById(DeleteUserByIdCommand deleteUserByIdCommand)
+    {
+        Result<string> result = await _mediator.Send(deleteUserByIdCommand);
+        return StatusCode(result.StatusCode, result);
+    }
+}
