373b3591ac9f4deb5cc67ac75e28e17650dba1d5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:33:26 +0300

    BusinessRules ile Identity ve NotFound kontrolü eklendi.

--- AppointmentSystemServer.Application/Features/Patients/Update/UpdatePatientCommandHandler.cs
+++ AppointmentSystemServer.Application/Features/Patients/Update/UpdatePatientCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AppointmentSystemServer.Application.Features.Patients._Constants;
+using AppointmentSystemServer.Application.Features.Patients._Rules;
 using AppointmentSystemServer.Application.Services.Repositories;
 using AppointmentSystemServer.Domain.Entities;
 using AppointmentSystemServer.Infrastructure.Caching;
@@ -13,19 +14,20 @@ public class UpdatePatientCommandHandler(
     IPatientRepository patientRepository,
     IUnitOfWork unitOfWork,
     IMapper mapper,
-    ICacheService cacheService) : IRequestHandler<UpdatePatientCommand, Result<string>>
+    ICacheService cacheService,
+    PatientBusinessRules patientBusinessRules) : IRequestHandler<UpdatePatientCommand, Result<string>>
 {
     public async Task<Result<string>> Handle(UpdatePatientCommand request, CancellationToken cancellationToken)
     {
         Patient? patient = await patientRepository.GetByExpressionAsync(d => d.Id == request.Id, cancellationToken);
-        if (patient is null) return Result<string>.Failure(PatientConstants.NotFound);
 
-        if (patient.IdentityNumber != request.IdentityNumber)
-        {
-            Boolean existPatient = patientRepository.Any(p => p.IdentityNumber == request.IdentityNumber);
-            if (existPatient) return Result<string>.Failure(PatientConstants.IdentityNumberAlreadyUse);
-        }
+        var validate = patientBusinessRules.NotFoundAndIdentityNumberAlreadyUse(patient, request);
+        return validate.IsSuccessful ? await ContinueProcess(request, patient) : validate;
+    }
 
+
+    private async Task<Result<string>> ContinueProcess(UpdatePatientCommand request, Patient patient)
+    {
         mapper.Map(request, patient);
         patientRepository.Update(patient);
         await unitOfWork.SaveChangesAsync();
