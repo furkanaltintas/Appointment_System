4b6514cfde824666a3b58ccb1fb850229c5c8eab
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:33:48 +0300

    BusinessRules eklendi ve Mappings güncellendi

--- AppointmentSystemServer.Application/Features/Patients/_Mappings/PatientMappingsProfiles.cs
+++ AppointmentSystemServer.Application/Features/Patients/_Mappings/PatientMappingsProfiles.cs
@@ -1,13 +1,14 @@
-﻿using AppointmentSystemServer.Application.Features.Patients.Create;
+﻿using AppointmentSystemServer.Application.Dtos;
+using AppointmentSystemServer.Application.Features.Patients.Create;
 using AppointmentSystemServer.Application.Features.Patients.Update;
 using AppointmentSystemServer.Domain.Entities;
 using AutoMapper;
 
 namespace AppointmentSystemServer.Application.Features.Patients._Mappings;
 
-public class PatientMappingsProfiles : Profile
+public class UserMappingsProfiles : Profile
 {
-    public PatientMappingsProfiles()
+    public UserMappingsProfiles()
     {
         CreateMap<Patient, CreatePatientCommand>()
             .ReverseMap();
@@ -16,5 +17,8 @@ public class PatientMappingsProfiles : Profile
             .ReverseMap()
             .ForMember(dest => dest.FullName, opt => opt.Ignore())
             .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+        CreateMap<Patient, PatientDto>()
+            .ReverseMap();
     }
 }
