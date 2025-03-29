a4fa0f77cde79bda503b5980f92c6735fafd1871
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:34:41 +0300

    Create User

---
+++ AppointmentSystemServer.Application/Features/User/Create/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.User.Create;
+
+public record CreateUserCommand(
+    string FirstName,
+    string LastName,
+    string Email,
+    string UserName,
+    string Password,
+    List<Guid> RoleIds) : IRequest<Result<string>>;
