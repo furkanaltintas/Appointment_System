eb20730c1e3e7456cede9dec81dfcd1db52c709c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:30:34 +0300

    DeleteAppointment

---
+++ AppointmentSystemServer.Application/Features/Appointments/DeleteById/DeleteAppointmentByIdCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.Appointments.DeleteById;
+
+public record DeleteAppointmentByIdCommand(int Id, int DoctorId) : IRequest<Result<string>>;
