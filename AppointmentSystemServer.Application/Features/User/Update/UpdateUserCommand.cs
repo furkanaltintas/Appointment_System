624857db1fadab54cfbb15f1c4233c1bfcb1fb79
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:35:02 +0300

    Update Use

---
+++ AppointmentSystemServer.Application/Features/User/Update/UpdateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace AppointmentSystemServer.Application.Features.User.Update;
+
+public record UpdateUserCommand(
+    Guid Id,
+        string FirstName,
+    string LastName,
+    string Email,
+    string UserName,
+    List<Guid> RoleIds) :  IRequest<Result<string>>;
