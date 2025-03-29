7b353b0bd8257d81e182ae815b394fbe05ab0ce4
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:31:05 +0300

    v1

--- AppointmentSystemServer.Application/Features/Appointments/UpdateAppointment/UpdateAppointmentCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using TS.Result;
-
-namespace AppointmentSystemServer.Application.Features.Appointments.UpdateAppointment;
-
-public record UpdateAppointmentCommand(
-    int Id,
-    string StartDate,
-    string EndDate) : IRequest<Result<string>>;
