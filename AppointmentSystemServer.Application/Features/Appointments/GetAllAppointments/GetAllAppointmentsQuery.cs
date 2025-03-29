060caa7e777b53fc9b825dec937734ede38f003d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-29 03:30:52 +0300

    v1

--- AppointmentSystemServer.Application/Features/Appointments/GetAllAppointments/GetAllAppointmentsQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using TS.Result;
-
-namespace AppointmentSystemServer.Application.Features.Appointments.GetAllAppointments;
-
-public record GetAllAppointmentsQuery(int DoctorId) : IRequest<Result<List<GetAllAppointmentsQueryResponse>>>;
